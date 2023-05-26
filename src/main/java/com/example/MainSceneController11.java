71623e6e3be5dae76f1694bcc8960fa4f98261b1
AnumBatool12 <a9batool@gmail.com>
2023-05-26 13:52:38 +0500

    created attendee list screen

---
+++ src/main/java/com/example/MainSceneController11.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class MainSceneController11 {
+
+    @FXML
+    private TextField GuestEmail;
+
+    @FXML
+    private TextField GuestlastName;
+
+    @FXML
+    private TextField GuestphoneNum;
+
+    @FXML
+    private Button Return;
+
+    @FXML
+    private Button addAttendee;
+
+    @FXML
+    private TableView<?> attendeeList;
+
+    @FXML
+    private CheckBox feepaid;
+
+    @FXML
+    private TextField guestfirstName;
+
+    @FXML
+    private Button removeSelected;
+
+}
