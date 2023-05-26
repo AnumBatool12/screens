ecc902227ca1a18fd2c82a01b89d6c48fb43e244
AnumBatool12 <a9batool@gmail.com>
2023-05-26 11:13:27 +0500

    editing MainSceneController1

--- src/main/java/com/example/MainSceneController1.java
+++ src/main/java/com/example/MainSceneController1.java
@@ -2,9 +2,14 @@ package com.example;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 
 public class MainSceneController1 {
 
     @FXML
     private Button LoginBtn;
+
+    @FXML
+    private Hyperlink signup;
+
 }
