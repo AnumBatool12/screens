db56fd22466f4bba543f73e937561cd6590aed26
AnumBatool12 <a9batool@gmail.com>
2023-05-28 21:52:33 +0500

    figured out how to change screens

--- src/main/java/com/example/MainSceneController1.java
+++ src/main/java/com/example/MainSceneController1.java
@@ -1,11 +1,32 @@
 package com.example;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class MainSceneController1 {
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void goToSignUpPage(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController2.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+   
     @FXML
     private Button LoginBtn;
 
