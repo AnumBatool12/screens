3b316b919354cadb6864c385ee8247ed163bf3a9
AnumBatool12 <a9batool@gmail.com>
2023-05-28 20:25:36 +0500

    made new screens

---
+++ src/main/java/com/example/MainSceneController26.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class MainSceneController26 {
+
+    @FXML
+    private Button addTaskBtn;
+
+    @FXML
+    private TableColumn<?, ?> budgetList;
+
+    @FXML
+    private TextField price;
+
+    @FXML
+    private Button removeBtn;
+
+    @FXML
+    private Button returnBtn;
+
+    @FXML
+    private TextField task;
+
+    @FXML
+    private TableView<?> taskList;
+
+}
