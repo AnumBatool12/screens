6a7b84ea69a7431262bc09d1a1cc619b474c022e
AnumBatool12 <a9batool@gmail.com>
2023-05-29 00:38:27 +0500

    Linked the screens

--- src/main/java/com/example/MainSceneController1.java
+++ src/main/java/com/example/MainSceneController1.java
@@ -18,6 +18,16 @@ public class MainSceneController1 {
     private Scene scene;
     private Parent root;
 
+    // Sign Up options for different types of actors
+    public void openLoginPage(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController1.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    // Sign Up options for different types of actors
     public void goToSignUpPage(ActionEvent event) throws IOException{
         root=FXMLLoader.load(getClass().getResource("MainSceneController2.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -25,8 +35,236 @@ public class MainSceneController1 {
         stage.setScene(scene);
         stage.show();
     }
-    
-   
+
+    //Open Sign Up Form for Client
+    public void openSignUpForm_Client(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController3.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Sign up Form for Event Planner
+    public void openSignUpForm1_EventPlanner(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController4.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Final SignUp Form for Event Planner
+    public void openSignUpForm2_EventPlanner(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController5.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Sign Up Form for Logistic Service
+    public void openSignUpFormLogisticService(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController6.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Dashboard for Client
+    public void openClientDashboard(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController7.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Create Event Option
+    public void openCreateEventForm(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController8.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open DashBoard For Event
+    public void openEventDashboard(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController9.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Form to Change Client's Personal Profile Information
+    public void openProfilePage_Client(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController10.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Form to Create/Update Attendee List
+    public void openAttendeeList(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController11.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Form to Create/Update To Do List
+    public void openToDoList(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController12.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Opening Event Planner or Logistic Service Dashboard
+    //we might need an if-else here so that itll open the relevant profile page
+    public void openEP_LS_Dashboard(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController13.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Form to Chanege Event Planners Personal Information
+    public void openProfilePage_EP(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController14.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Opening Event Dashboard for Event Planner
+    public void openEventDashboard_EP(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController15.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Opening Event Dashboard Logistics
+    public void openEventDashboard_LS(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController16.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Event Requests from Client to Event Planner or Logistic Service
+    public void openEventRequestForm(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController17.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Request List by Event Planner or Logistic Service
+    public void openEventRequestList(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController18.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Request by Event Planner or Logistics
+    public void openEventRequest(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController19.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Profile Change for Logistic Service
+    public void openProfilePage_LS(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController20.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Logistic's Service/Web Page
+    public void openServicePage_LS(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController21.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Form to Set Event Requirements
+    public void openEventReqirementsForm(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController22.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Search for Event Planner and Logistics
+    public void openSearch(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController23.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Event Planner Display Page
+    public void openSearch_EP(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController24.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Open Logistic Service Display Page
+    public void openSearch_LS(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController25.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Page to Create Budget
+    public void openCreateBudgetForm(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController26.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //Page to Create Bill
+    public void openCreateBillForm(ActionEvent event) throws IOException{
+        root=FXMLLoader.load(getClass().getResource("MainSceneController27.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+
+
+/*
     @FXML
     private Button LoginBtn;
 
@@ -34,4 +272,12 @@ public class MainSceneController1 {
     @FXML
     private Hyperlink signup;
 
-}
+    @FXML
+    private Button ClientRegister;
+
+    @FXML
+    private Button PlannerRegister;
+
+    @FXML
+    private Button ServiceRegister2;
+ */
