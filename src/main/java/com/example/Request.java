c4a5a4310ad9cb4315f474e04f47b0bcc0023893
AnumBatool12 <a9batool@gmail.com>
2023-05-29 23:50:02 +0500

    handled the request functionality

--- src/main/java/com/example/Request.java
+++ src/main/java/com/example/Request.java
@@ -1,10 +1,39 @@
 package com.example;
+
+import java.time.LocalDate;
+
 //Request
 public class Request {
-    private boolean status;
+    private Boolean status;
     private int reqNum;
+    private Event event;
+    private User user;
+    private int ID;
 
-    public void setStatus(boolean stat){status = stat;}
+    Request(){}
+
+    Request(Boolean stat, int num, Event event, User user, int id){
+        status = stat;
+        reqNum=num;
+        this.event=event;
+        this.user=user;
+        ID=id;
+    }
+
+    //setters
+    public void setStatus(Boolean stat){status = stat;}
+    public void setNum(int num) {reqNum=num;}
+    public void setID(int id) {ID=id;}
+
+    //getters
     public boolean getStatus(){return status;}
     public int getReqNum(){return reqNum;}
+    public String getTitle() {return event.getTitle();}
+	public LocalDate getDate() {return event.getDate();}
+	public String getTime() {return event.getTime();}
+	public Integer getSize() {return event.getSize();}
+    public String getFullname() {return user.getFullname();}
+	public String getEmailmail() {return user.getEmailmail();}
+	public String getPhoneNo(){return user.getPhoneNo();}
+    public int getID(){return ID;}
 }
---
+++ src/main/java/com/example/RequestList.java
@@ -0,0 +1,52 @@
+package com.example;
+
+public class RequestList {
+	
+
+	public void getRequest(Object LogisticName) {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	
+	public void getEventRequest(Object RequestNo, Object LogisticName) {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	
+	public void manageEventRequest(Object RequestNo, Object decision) {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	
+	public void getClient(Object LogisticName) {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	
+	public void getEventDate(Object RequestNo) {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	
+	public void updateRequirements(Object RequestNo, Object reqs) {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+}
