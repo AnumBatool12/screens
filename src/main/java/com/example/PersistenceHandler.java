c481159ebad27eae92238dc4d6f2c4a28e40b532
SyedaFatima002 <syedafatima0998@gmail.com>
2023-05-29 16:12:11 +0500

    updated persistance handler

--- src/main/java/com/example/PersistenceHandler.java
+++ src/main/java/com/example/PersistenceHandler.java
@@ -1,6 +1,8 @@
 package com.example;
 
 public interface PersistenceHandler{
+
+    PersistenceHandler getInstance();
     boolean verifyExistence(String username);
     boolean checkPassword(String username, String password);
     char userIS(String usernamae);
