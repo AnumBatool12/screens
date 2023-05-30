93c68a7bb470b72fea3ae19e7c68aec03fcaa893
AnumBatool12 <a9batool@gmail.com>
2023-05-30 12:33:31 +0500

    some more

--- src/main/java/com/example/MySqlClass.java
+++ src/main/java/com/example/MySqlClass.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-
+/* 
 public class MySqlClass {
     private Connection cn;
     private static MySqlClass instance;
